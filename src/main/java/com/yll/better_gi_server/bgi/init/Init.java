5b559d37f85727c14682165799717dd88c92e8e1
夜林蓝 <1562098029@qq.com>
2025-03-27 18:18:14 +0800

    1.加入日志 2.解决打包后，文件读不到的问题。

--- src/main/java/com/yll/better_gi_server/bgi/init/Init.java
+++ src/main/java/com/yll/better_gi_server/bgi/init/Init.java
@@ -2,6 +2,7 @@ package com.yll.better_gi_server.bgi.init;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
 import com.yll.better_gi_server.bgi.config.LogFileConfig;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.InputStream;
 
 /**
  *@项目名称: better_gi_server
@@ -30,12 +35,23 @@ public class Init implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String nircmd = ResourceUtil.getResource("script/nircmd.exe").getPath();
-		log.info("【better gi】 回调接口是：http://{}:{}{}/{}", logFileConfig.getIp(), serverPort, contextPath, "shutdown");
+		String script = System.getProperty("user.dir") + File.separator + "nircmd.exe";
+		if (!FileUtil.exist(script)) {
+			log.info(script);
+			log.info("正在生成...");
+			try (InputStream inputStream = ResourceUtil.getResource("script/nircmd.exe").openStream();
+					BufferedOutputStream outputStream = FileUtil.getOutputStream(script);) {
+				IoUtil.copy(inputStream, outputStream);
+			}
+		}
+		log.info("【better gi】 回调接口是：http://{}:{}{}/{}", logFileConfig.getIp(), serverPort, contextPath,
+				"shutdown");
 		//删除昨天的日志文件
-		FileUtil.del(logFileConfig.getPath() + DateUtil.format(DateUtil.yesterday(), logFileConfig.getTimeStamp()) + logFileConfig.getSuffix());
+		FileUtil.del(logFileConfig.getPath() + DateUtil.format(DateUtil.yesterday(), logFileConfig.getTimeStamp())
+				+ logFileConfig.getSuffix());
 		//获取当前日期，格式为 yyyymmdd
-		String fileName = logFileConfig.getPath() + DateUtil.format(DateUtil.date(), logFileConfig.getTimeStamp()) + logFileConfig.getSuffix();
+		String fileName = logFileConfig.getPath() + DateUtil.format(DateUtil.date(), logFileConfig.getTimeStamp())
+				+ logFileConfig.getSuffix();
 		//检查文件是否存在
 		if (!FileUtil.exist(fileName)) {
 			//不存在则创建
@@ -48,12 +64,12 @@ public class Init implements CommandLineRunner {
 			//文件fileName创建成功
 			log.info("文件{}创建成功", fileName);
 			//静音
-			Runtime.getRuntime().exec(nircmd+" mutesysvolume 1");
+			Runtime.getRuntime().exec("nircmd.exe mutesysvolume 1");
 			//启动BGI 一条龙程序。"C:\Program Files\BetterGI\BetterGI.exe" startOneDragon
 			Runtime.getRuntime().exec(logFileConfig.getStartScript());
 		} else {
 			// 文件存在，不静音
-			Runtime.getRuntime().exec(nircmd+" mutesysvolume 0");
+			Runtime.getRuntime().exec("nircmd.exe mutesysvolume 0");
 			log.info("文件{}已存在（今日任务已经执行）", fileName);
 			log.info("{}秒后，将自动关闭窗口", logFileConfig.getCloseSecond());
 			//睡眠1min
